cd662a036587fe51d08468cd127d7da2d3d2b067
hudengjun <hudengjun@corp.netease.com>
2022-04-28 11:36:02 +0800

    comment the ss

--- include/rtd/waitgroup.h
+++ include/rtd/waitgroup.h
@@ -1,68 +1,66 @@
 #ifndef RTDSYNC_WAITGROUP_H
 #define RTDSYNC_WAITGROUP_H
 
-#include <stdexcept>
-#include <condition_variable>
 #include <atomic>
+#include <condition_variable>
 #include <iostream>
 #include <sstream>
+#include <stdexcept>
 
 namespace rtd {
 
 class WaitGroup {
 protected:
-    WaitGroup() : c_(0), w_(0) {}
+  WaitGroup() : c_(0), w_(0) {}
 
 public:
-    friend std::shared_ptr<WaitGroup> MakeWaitGroup();
+  friend std::shared_ptr<WaitGroup> MakeWaitGroup();
 
-    // Add 1 before creating a async task.
-    void Add(int delta) {
-        if(c_ < 0) {
-            throw std::logic_error("negative WaitGroup counter");
-        }
-        c_ += delta;
-        std::stringstream ss;
-        ss << "c_:" << c_ << "   w_:" << w_ << std::endl;
-        std::cout << ss.str();
-        if(c_ > 0 || w_ == 0) {
-            return;
-        }
-
-        for(; w_ != 0; w_--) {
-            cv_.notify_one();
-        }
+  // Add 1 before creating a async task.
+  void Add(int delta) {
+    if (c_ < 0) {
+      throw std::logic_error("negative WaitGroup counter");
+    }
+    c_ += delta;
+    // std::stringstream ss;
+    // ss << "c_:" << c_ << "   w_:" << w_ << std::endl;
+    // std::cout << ss.str();
+    if (c_ > 0 || w_ == 0) {
+      return;
     }
 
-    // Done() after complete a async task.
-    void Done() {
-        Add(-1);
+    for (; w_ != 0; w_--) {
+      cv_.notify_one();
     }
+  }
 
-    // Wait() can blocking, until all task being done.
-    void Wait() {
-        if(c_ == 0) {
-            return;
-        }
-        w_ += 1;
-        std::unique_lock<std::mutex> lc(mu_);
-        cv_.wait(lc, [&]() { return c_ == 0; });
+  // Done() after complete a async task.
+  void Done() { Add(-1); }
+
+  // Wait() can blocking, until all task being done.
+  void Wait() {
+    if (c_ == 0) {
+      return;
     }
+    w_ += 1;
+    std::unique_lock<std::mutex> lc(mu_);
+    cv_.wait(lc, [&]() { return c_ == 0; });
+  }
 
 private:
-    std::atomic<int> w_; // waiter
+  std::atomic<int> w_; // waiter
 
-    std::atomic<int> c_; // counter
+  std::atomic<int> c_; // counter
 
-    std::mutex mu_;
+  std::mutex mu_;
 
-    std::condition_variable cv_;
+  std::condition_variable cv_;
 };
 
 std::shared_ptr<WaitGroup> MakeWaitGroup() {
-    return std::shared_ptr<WaitGroup>(new WaitGroup());
+  return std::shared_ptr<WaitGroup>(new WaitGroup());
 }
 
-}
+} // namespace rtd
 
-#endif //RTDSYNC_WAITGROUP_H
+#endif // RTDSYNC_WAITGROUP_H
